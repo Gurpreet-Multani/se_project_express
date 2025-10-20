846b7fce99984ffe2695192ed844b0cc8268f13c
Gurpreet-Multani <gmultani133@gmail.com>
2025-10-19 19:43:15 -0700

    Sprint 13 ver4 lint and action

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // imports up here---------------------------------------------------------------
-const express = require("express");
-const mongoose = require("mongoose");
-const mainRouter = require("./routes");
-const cors = require("cors");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const mainRouter = require('./routes');
 // imports up here--------------------------------------------------------------
 
 // express
@@ -12,22 +12,22 @@ const { PORT = 3001 } = process.env;
 // converts our document to json so postman can understand
 app.use(express.json()); // For parsing application/json
 
-//allows request from client to the server to be processed
+// allows request from client to the server to be processed
 app.use(cors());
 
 // MongoDb/mongoose
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
 
 // This middleware is for the GitHub Actions tests to pass.
 // It sets a hardcoded user ID for requests that don't have a JWT.
 app.use((req, res, next) => {
   req.user = {
-    _id: "5d8b8592978f8bd833ca8133", // test user _id
+    _id: '5d8b8592978f8bd833ca8133', // test user _id
   };
   next();
 });
 // users.js Routes
-app.use("/", mainRouter);
+app.use('/', mainRouter);
 
 // app/listen
 app.listen(PORT, () => {});
